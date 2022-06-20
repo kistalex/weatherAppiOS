dbd5509b83bc1edef5fd69b0289ccfd3c2c743a1
Alexander <kistalexander@yandex.ru>
2022-06-20 19:42:51 +0400

    Task 1

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -7,13 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDataSource {
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+         return 10
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                 
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "weatherCell", for: indexPath)
+                    
+            cell.backgroundColor = .red
+            return cell
+    }
+    
+    @IBOutlet weak var weatherCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        weatherCollectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "weatherCell")
+    
+        weatherCollectionView.delegate = self
+        weatherCollectionView.dataSource = self
     }
-
-
 }
 
+extension ViewController: UICollectionViewDelegateFlowLayout {
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let screenSize = UIScreen.main.bounds
+        return CGSize(width: screenSize.width, height: screenSize.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return .zero
+    }
+}
